72b214aadaef6731594d8e218829811216819cbe
James Holt <james.palmer.holt@gmail.com>
2020-03-01 21:25:20 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='ootables',
+    version='0.1.0',
+    packages=['ootables'],
+    install_requires=['lxml==4.5.0'],
+    author='James Holt',
+    author_email='holtjp@outlook.com',
+    description='An API for reading data from Microsoft Excel Tables',
+    keywords='Microsoft Office Open Table Excel',
+    url='https://github.com/holtjp/ootables',
+    license='BSD 3-Clause License'
+)
